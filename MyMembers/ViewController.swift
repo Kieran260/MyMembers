58438da565d051f792528af9ec19c5ad26ca78ac
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-02-05 19:17:06 +0000

    Initial Commit

---
+++ MyMembers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMembers
+//
+//  Created by Kieran Hardwick on 05/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
